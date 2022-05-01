d1dc96b5d9cdf8509fd3bab5ab323a1c89cd9606
ErikaDeger <erikadeger94@gmail.com>
2022-05-01 20:20:48 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -240,7 +240,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+
+  if(letra.lenght > 1) {
+  return "Dato incorrecto"
+}
+    if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+    return "Es vocal"
+  }
+    return "Dato incorrecto"
+
 }
 
 
